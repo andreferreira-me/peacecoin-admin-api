b6b74b0d6759b6648d35a03572f8e1cbcea248c5
Ibraim Mansur <imansur@gmail.com>
2016-04-28 18:47:00 -0300

    Implematção da API

--- both/api/api.js
+++ both/api/api.js
@@ -2,8 +2,33 @@
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    collectionApi = new CollectionAPI({ authToken: '97f0ad9e24ca5e0408a269748d7fe0a0' });
-    collectionApi.addCollection(Clients, 'clients');
+    collectionApi = new CollectionAPI({
+      authToken: undefined,              // Require this string to be passed in on each request
+      apiPath: 'smartcontrats',          // API path prefix
+      listenPort: 3005,                  // Port to listen to (stand-alone only)
+      listenHost: undefined,             // Host to bind to (stand-alone only)
+      timeOut: 120000
+    });
+    collectionApi.addCollection(Clients, 'clients',{
+      authenticate: function(token, method, requestMetadata) {
+
+        debugger
+        console.log("authen");
+        console.log("token: " + token);
+        console.log("method: " + method);
+        console.log("requestMetadata: " + JSON.stringify(requestMetadata));
+        if (token === undefined) {
+          return false;
+        }
+
+        var client = Clients.findOne({"Token":token} );
+
+        if (!client) {
+          return false;
+        }
+        return true;
+      }
+    });
     collectionApi.start();
   });
 }
